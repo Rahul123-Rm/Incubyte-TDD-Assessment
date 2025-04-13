8aadb505cbdb1a5934fe7a4604cf8601548b6ec3
Rahul123-Rm <rahulmeena4767@gmail.com>
2025-04-14 03:28:11 +0530

    Handle multiple comma-separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,8 @@
 # string_calculator.rb
 class StringCalculator
-  return 0 if input.strip.empty?
-  return input.to_i if !input.include?(",")
-
-  numbers = input.split(",").map(&:to_i)
-  numbers.sum
+  def add(input)
+   return 0 if input.strip.empty?
+    numbers = input.split(",").map(&:to_i)
+    numbers.sum
+  end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -8,10 +8,15 @@ RSpec.describe StringCalculator do
   end
 
   it "returns the number itself when one number is given" do
-  expect(calculator.add("1")).to eq(1)
+    expect(calculator.add("1")).to eq(1)
+  end
 
   it "returns sum of two comma-separated numbers" do
-   expect(calculator.add("1,2")).to eq(3)
+    expect(calculator.add("1,2")).to eq(3)
+  end
+
+  it "returns sum of multiple comma-separated numbers" do
+   expect(calculator.add("1,2,3,4")).to eq(10)
   end
   
 end
